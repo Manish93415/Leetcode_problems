dc1b84a39105999132f7866e4e31cd9311525aef
Manish Singh <singhmanish8397@gmail.com>
2026-06-30 10:43:16 +0530

    Time: 0 ms (100.00%) | Memory: 22.4 MB (89.34%) - LeetSync

---
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+
+    pair<int,int> postorder(TreeNode* root){
+        if(root==NULL) return {0,0};
+        pair<int,int> left=postorder(root->left);
+        pair<int,int> right= postorder(root->right);
+        int included=root->val +left.second + right.second;
+        int excluded= max(left.first , left.second) + max(right.first,right.second);
+        return {included,excluded};
+    }
+
+    int rob(TreeNode* root) {
+        
+        pair<int,int> ans=postorder(root);
+        return max(ans.first,ans.second);
+    }
+};
