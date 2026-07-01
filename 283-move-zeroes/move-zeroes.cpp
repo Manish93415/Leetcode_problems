aff59b22a208d6900a2341afa6b1edd8c971b6f5
Manish Singh <singhmanish8397@gmail.com>
2026-07-01 20:27:41 +0530

    Time: 3 ms (14.71%) | Memory: 24 MB (19.08%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int first=0,second=1;
-        while(second<nums.size()){
-            if(nums[first]==0 && nums[second]==0){
-                second++;
+        // Pointer to the first zero
+        int j = -1;
+
+        // Find the first zero
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
             }
-            else if(nums[first]==0 && nums[second]!=0){
-                swap(nums[first],nums[second]);
-                first++,second++;
-            }       
-            else{
-                first++,second++;
-            }
-        
         }
 
-        for(int num:nums){
-            cout<<num<<" ";
+        // If no zero found, return
+        if (j == -1) return;
+
+        // Start from the next index of first zero
+        for (int i = j + 1; i < nums.size(); i++) {
+            // If current element is non-zero
+            if (nums[i] != 0) {
+                // Swap with nums[j]
+                swap(nums[i], nums[j]);
+                // Move j to next zero
+                j++;
+            }
         }
     }
 };
