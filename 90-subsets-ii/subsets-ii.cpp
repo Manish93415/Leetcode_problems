85aba215a7a01e5909dbe16df1176df19db8e714
Manish Singh <singhmanish8397@gmail.com>
2026-07-11 10:20:10 +0530

    Time: 2 ms (40.19%) | Memory: 10.5 MB (67.79%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    void find(vector<int> &nums,set<vector<int>> &ans, vector<int> &temp, int i){
-        if(i==nums.size()){
-            ans.insert(temp);
-            return;
-        }
+    void find(vector<int> &nums,
+          vector<vector<int>> &ans,
+          vector<int> &temp,
+          int index)
+{
+    ans.push_back(temp);
 
-        //include
+    for(int i = index; i < nums.size(); i++)
+    {
+        if(i > index && nums[i] == nums[i-1])
+            continue;
+
         temp.push_back(nums[i]);
-        find(nums,ans,temp,i+1);
 
-        // backtrack
+        find(nums, ans, temp, i + 1);
+
         temp.pop_back();
-
-        //exclude
-        find(nums,ans,temp,i+1);
     }
+}
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end()); 
-        set<vector<int>>fake;
+        // set<vector<int>>fake;
+        vector<vector<int>> ans;
         vector<int> temp;
         int i=0;
-        find(nums,fake,temp,i);
-        vector<vector<int>> ans(fake.begin(), fake.end());
+        find(nums,ans,temp,i);
+        
         return ans;
     }
 };
