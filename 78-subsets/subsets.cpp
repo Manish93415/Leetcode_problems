ef8fac8687539b996468b7610dbc3b3e566a98c0
Manish Singh <singhmanish8397@gmail.com>
2026-07-11 09:16:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (96.42%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void find(vector<int>& nums, vector<int>& temp,
+              vector<vector<int>>& ans, int i)
+    {
+        if(i == nums.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+
+        // Include
+        temp.push_back(nums[i]);
+        find(nums, temp, ans, i + 1);
+
+        // Backtrack
+        temp.pop_back();
+
+        // Exclude
+        find(nums, temp, ans, i + 1);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans; // stores all subsets 
+        vector<int> temp;  // stores current subset
+
+        find(nums, temp, ans, 0);
+
+        return ans;
+    }
+};
