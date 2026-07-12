7d625df37951ce16726ecf02fd82c2f481c01a10
Manish Singh <singhmanish8397@gmail.com>
2026-07-12 11:19:57 +0530

    Time: 4 ms (33.21%) | Memory: 10.2 MB (78.88%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    bool issafe(vector<string> &board,int row,int col,int n){
-        //horizintal
+    bool issafe(vector<string> &board,int row,int col,int n){ // for checking the safety of queens
+        //horizintal check
         for(int j=0;j<n;j++){
             if(board[row][j]=='Q') return false;
         }
 
-        //vertical
+        //vertical check
         for(int i=0;i<n;i++){
             if(board[i][col]=='Q') return false;
         }
 
-        //left diagonal
+        //left diagonal check 
         for(int i=row,j=col;i>=0 && j>=0; i--,j--){
             if(board[i][j]=='Q') return false;
         }
 
-        //right diagonal
+        //right diagonal check
         for(int i=row,j=col;i>=0 && j<n; i--,j++){
             if(board[i][j]=='Q') return false;
         }
@@ -24,7 +24,7 @@ public:
         return true;
     }
 
-    void nqueens(vector<string> &board,int row,int n,vector<vector<string>> &ans){
+    void nqueens(vector<string> &board,int row,int n,vector<vector<string>> &ans){  // for placing queens
 
         if(row==n){
             ans.push_back({board});
@@ -36,13 +36,13 @@ public:
             if(issafe(board,row,j,n)){
                 board[row][j] = 'Q';
                 nqueens(board,row+1,n,ans);
-                board[row][j]='.';
+                board[row][j]='.';  //backtrack 
 
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n,string(n,'.'));
+        vector<string> board(n,string(n,'.'));  //initialize board with n dots in rows and columns
         vector<vector<string>> ans;
         nqueens(board,0,n,ans);
         return ans;
